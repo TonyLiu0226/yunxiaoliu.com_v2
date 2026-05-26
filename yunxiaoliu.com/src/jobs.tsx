5408c0b2a71379d0ac7acdaf6d518cc980a93cfe
TonyLiu0226 <TL0226yn@gmail.com>
2026-05-25 20:32:29 -0700

    add jobs

---
+++ yunxiaoliu.com/src/jobs.tsx
@@ -0,0 +1,121 @@
+import type { Job } from "./types";
+
+export const jobs: Job[] = [
+    {
+        company: "FlyOver Canada",
+        role: "Guest Experience Guide",
+        startDate: "May 2022",
+        endDate: "Sep 2022",
+        location: "Vancouver, BC",
+        description: `Worked as a Guest Experience Guide at Flyover Canada, a 4D flight simulation experience in the Canada Place cruise ship terminal.
+
+            - Worked a variety of stations such as selling tickets, editing and selling photos, and keeping track of retail inventory.
+
+            - Scanned tickets and sorted customers into groups for timed entry to ensure a smooth experience for all guests.
+
+            - Guided groups of up to 61 people through the entire experience, from preshow to safety demo to ride to exit, adhering to proper safety protocols.
+
+            - Communicated effectively with guests and team members in a fast-paced environment to provide a memorable experience to thousands of people every day.`
+    },
+    {
+        company: "The Big Leaf",
+        role: "Frontend Developer",
+        startDate: "Jun 2022",
+        endDate: "Sep 2022",
+        location: "Remote",
+        description: `Worked on two projects during my part-time internship:
+
+            - Building a React Native app for MyPanion, a healthcare startup
+
+            - Redesigning handlers.ca to use React to improve responsiveness and usability`
+    },
+    {
+        company: "Invoke Media",
+        role: "Fullstack Developer",
+        startDate: "Jan 2023",
+        endDate: "Apr 2023",
+        location: "Vancouver, BC",
+        description: `Full-time internship where I focused on developing frontend and backend features for Incrowd, a multiplatform app for recording and streaming live events.
+
+        Utilized a wide variety of languages, frameworks, and tools, such as TypeScript, React, GraphQL, AWS Lambda, S3, DynamoDB, and ElastiCache.`
+    },
+    {
+        company: "FlyOver Canada",
+        role: "Guest Experience Guide",
+        startDate: "May 2023",
+        endDate: "Dec 2023",
+        location: "Vancouver, BC",
+        description: `I came back LMAO...`
+    },
+    {
+        company: "BoardSpace Inc.",
+        role: "Software Developer (AI)",
+        startDate: "Dec 2023",
+        endDate: "Feb 2024",
+        location: "Remote",
+        description: `As part of the Riipen Level Up program, worked on a chatbot to improve customer support for a condo management platform.
+
+            - Leveraged OpenAI's GPT-4-turbo LLM to generate responses to common questions.
+
+            - Developed a Python Flask API to connect the chatbot to the condo management platform.
+
+            - Utilized the LangChain framework with OpenAI Embeddings API and FAISS vector database to implement retrieval augmented generation on support website pages.
+
+            - Created a Python script to automatically generate tags for new document uploads, eliminating the need for manual curation.`
+    },
+    {
+        company: "Invoke Media",
+        role: "Fullstack Developer",
+        startDate: "Jun 2024",
+        endDate: "Aug 2024",
+        location: "Vancouver, BC",
+        description: `I came back yet again LMAO....
+
+        But fr though, this time I had much more responsibilities, working on six projects during my return internship.
+
+            - Learned about the importance of thorough unit and end-to-end testing, spearheading the implementation of test suites for new features.
+
+            - Developed frontend and backend features for client facing applications with minimal guidance in a fast-paced timeline, regularly syncing with stakeholders for feedback.
+
+            - Leveraged many technologies not touched on in my previous internship, such as Next.js, Nest.js, Docker, PHP, and PostgreSQL.`
+    },
+    {
+        company: "NETINT Technologies Inc.",
+        role: "Software Engineer",
+        startDate: "Sep 2024",
+        endDate: "Apr 2025",
+        location: "Burnaby, BC",
+        description: `More AI and LLM stuffs, basically building and improving upon ChatGPT clones for internal use.
+        - Developed internal tooling such as chatbots, coding agents, and RAG-based tools to improve productivity and workflows for 50+ employees.
+        - Built infrastructure using PyTorch and TensorFlow to train latent video compression CNNs on 80,000 multi-frame sequences, and developed Python Scripts
+        to evaluate and visualize model performance on datasets consisting of 600-frame sequences.
+        - Built a dashboard to track employee LLM usage, enabling data-driven optimizations that reduced API costs by 50%.
+        `
+    },
+    {
+        company: "University of British Columbia",
+        role: "Project Manager",
+        startDate: "May 2025",
+        endDate: "May 2026",
+        location: "Vancouver, BC",
+        description: `I was responsible for overseeing the redesign and redevelopment of UBC Botanical Garden's website, used by more than 100,000 users annually,  
+        managing timelines, scope, requirements, and deliverables among various stakeholders and co-ordinating with a third party agency to execute site development.
+
+        - Conducted user interviews with 10+ stakeholders to create prioritized user stories that informed sitemap and UI decisions.
+        - Led usability testing with 15+ staff to validate core features, identify usability gaps, and drive post-launch improvements.
+        - Defined success metrics using Google Analytics, establishing KPIs (bounce rate, session duration, page interactions) to measure
+post-launch performance and guide content development.    
+        `
+    },
+    {
+        company: "Nutanix",
+        role: "Software Engineer",
+        startDate: "July 2026",
+        endDate: "",
+        location: "Vancouver, BC",
+        description: `
+        Coming soon... Stay tuned!!!
+        `
+    }
+    
+]
---
+++ yunxiaoliu.com/src/types.ts
@@ -0,0 +1,21 @@
+export type Job = {
+    company: string;
+    role: string;
+    startDate: string;
+    endDate: string;
+    location: string;
+    description: string;
+}
+
+export type Project = {
+    name: string;
+    description: string;
+    GitHubURL: string;
+}
+
+export type Education = {
+    school: string;
+    degree: string;
+    startDate: string;
+    endDate: string;
+}
