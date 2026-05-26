b13bbaaf4b99b77845f81308c96972216df1560a
TonyLiu0226 <TL0226yn@gmail.com>
2026-05-25 20:38:48 -0700

    add projects

---
+++ yunxiaoliu.com/src/projects.tsx
@@ -0,0 +1,34 @@
+import type { Project } from "./types";
+
+export const projects: Project[] = [
+    {
+        name: "Flowleaflets",
+        description: "Developed in collaboration with a nonprofit, Flowleaflets is a SaaS that allows medical students to transcribe and digitize handwritten notes from medical logbooks.",
+        GitHubURL: "https://github.com/ubclaunchpad/clinical-logging"
+    },
+    {
+        name: "Class of 19",
+        description: "A visual novel made using RenPy about the experiences of two high school friends as they navigate the complexities of high school life. Totally NOT based on a true story in my life.",
+        GitHubURL: "https://github.com/TonyLiu0226/Class_of_19"
+    },
+    {
+        name: "Celeste.png",
+        description: "An app that leverages open-source LLMs hosted locally to generate short stories based on user-controlled prompts and togglable parameters.",
+        GitHubURL: "https://github.com/TonyLiu0226/celeste.png"
+    },
+    {
+        name: "EpiLog",
+        description: "A seizure tracker app for iOS and Android. Users can log daily seizure activity, record seizures via the camera, and keep track of important events and medications on a built-in calendar.",
+        GitHubURL: "https://github.com/ubclaunchpad/epilog"
+    },
+    {
+        name: "Shopinder",
+        description: "An app that allows you to make shopping lists ahead of time and set email or text reminders to notify you when its time to shop!",
+        GitHubURL: "https://devpost.com/software/shopinder"
+    },
+    {
+        name: "UniMeetups",
+        description: "A meetup app for university students to plan events and make friends with others who are geographically close and share common interests.",
+        GitHubURL: "https://devpost.com/software/unimeetups"
+    }
+]
